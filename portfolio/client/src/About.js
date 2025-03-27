077b1d5d9fa09f5e8d3012bf8fad41dadc2e9f3d
vaish621 <122389160+vaish621@users.noreply.github.com>
2025-03-27 13:08:12 +0530

    Update About.js

--- portfolio/client/src/About.js
+++ portfolio/client/src/About.js
@@ -59,7 +59,7 @@ const About = () => {
         
     </div>
 
-    <div className="bg-pink-950">
+    <div className="bg-black">
       <div className="max-w-4xl mx-auto px-4 py-12">
         <h2 className="text-teal-100 text-3xl md:text-4xl lg:text-5xl font-bold text-center mb-6">About Me</h2>
         <hr className="border-gray-500 my-6" />
